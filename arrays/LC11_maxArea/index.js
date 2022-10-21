e24e8f40ee8281f2793f911bf87bf95c61ce26e6
Vladyslav Sharapat <>
2022-10-21 11:05:25 +0300

    Solved 11 task;

--- arrays/LC11_maxArea/index.js
+++ arrays/LC11_maxArea/index.js
@@ -1,5 +1,22 @@
 //dev.to/seanpgallivan/solution-container-with-most-water-1907
 
-https: function maxArea(height) {}
+/**
+ * Time complexity O(n)
+ * Function that calculate max available volume
+ * @param {number[]} height
+ */
+function maxArea(height) {
+  let left = 0,
+    right = height.length - 1,
+    area = 0,
+    min = 0;
+
+  while (left < right) {
+    min = (right - left) * Math.min(height[left], height[right]);
+    area = Math.max(area, min);
+    height[left] < height[right] ? left++ : right--;
+  }
+  return area;
+}
 
 module.exports = maxArea;
