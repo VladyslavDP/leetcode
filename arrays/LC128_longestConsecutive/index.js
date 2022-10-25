2e52c9c6392d58c9d5c8120317688e4abdb71051
Vladyslav Sharapat <>
2022-10-25 21:21:10 +0300

    Solved 128 task;

--- arrays/LC128_longestConsecutive/index.js
+++ arrays/LC128_longestConsecutive/index.js
@@ -1,3 +1,56 @@
-function longestConsecutive(nums) {}
+/**
+ * Time complexity - O(n)
+ * Function that return longestConsecutive,
+ * e.g. [100,4,200,1,3,2] returns [1,2,3,4]
+ * @param {number[]} nums
+ */
+function longestConsecutive_(nums) {
+  let max = 1;
+  if (nums.length === 0) return 0;
+  const set = new Set(nums);
+  for (const num of nums) {
+    // looking for start point
+    if (set.has(num - 1)) continue;
+
+    let n = num;
+    let streak = 1;
+
+    while (set.has(n + 1)) {
+      n += 1;
+      streak += 1;
+      max = Math.max(max, streak);
+    }
+  }
+
+  return max;
+}
+
+/**
+ * Time complexity - O(N * log N)
+ * Function that return longestConsecutive,
+ * e.g. [100,4,200,1,3,2] returns [1,2,3,4]
+ * @param {number[]} nums
+ */
+function longestConsecutive_(nums) {
+  let res = nums.length === 0 ? 0 : 1;
+  let temp = nums.length === 0 ? 0 : 1;
+
+  nums.sort((a, b) => a - b);
+  nums = Array.from(new Set(nums));
+
+  for (i = 0; i < nums.length - 1; i++) {
+    const currVal = Number(nums[i]);
+    const nextVal = Number(nums[i + 1]);
+
+    if (nextVal - currVal === 1) {
+      temp += 1;
+      res = Math.max(temp, res);
+    } else {
+      temp = 1;
+    }
+  }
+
+  return res;
+}
 
 module.exports = longestConsecutive;
--- arrays/LC128_longestConsecutive/test.js
+++ arrays/LC128_longestConsecutive/test.js
@@ -1,7 +1,10 @@
-const longestConsecutive = require('./index');
+const longestConsecutive = require("./index");
 
-test('longestConsecutive works', () => {
-	expect(longestConsecutive([ 100, 4, 200, 1, 3, 2 ])).toEqual(4);
-	expect(longestConsecutive([ 100, 4, 200, 1, 2 ])).toEqual(2);
-	expect(longestConsecutive([ 0, -1 ])).toEqual(2);
+test("longestConsecutive works", () => {
+  expect(longestConsecutive([100, 4, 200, 1, 3, 2])).toEqual(4);
+  expect(longestConsecutive([100, 4, 200, 1, 2])).toEqual(2);
+  expect(longestConsecutive([0, -1])).toEqual(2);
+  expect(longestConsecutive([9, 1, 4, 7, 3, -1, 0, 5, 8, -1, 6])).toEqual(7);
+  expect(longestConsecutive([-1, -1, 0, 1, 3, 4, 5, 6, 7, 8, 9])).toEqual(7);
+  expect(longestConsecutive([1, 2, 0, 1])).toEqual(3);
 });
