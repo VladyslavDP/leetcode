712e52776b262f569ff4108b3249f758b46f69cd
Vladyslav Sharapat <>
2022-11-09 21:46:24 +0200

    Solved 33 task;

--- arrays/LC33_search/index.js
+++ arrays/LC33_search/index.js
@@ -1,3 +1,41 @@
-function search(nums, target) {}
+/**
+ * Function that search number in sorted array
+ * Time complexity O(log n)
+ * @param {number[]} nums
+ * @param {number} target
+ */
+function search(nums, target) {
+  if (nums.length === 0) return -1;
+  let left = 0,
+    right = nums.length - 1;
+
+  const isBeetween = (num1, num2, target) => num1 <= target && num2 >= target;
+
+  while (left <= right) {
+    let pivot = Math.floor((left + right) / 2);
+
+    if (nums[pivot] === target) return pivot;
+
+    // it's sorted
+    if (nums[left] <= nums[pivot]) {
+      // search in range
+      if (isBeetween(nums[left], nums[pivot], target)) {
+        right = pivot - 1;
+      }
+      // search in another subArray
+      else {
+        left = pivot + 1;
+      }
+    } else {
+      if (isBeetween(nums[pivot], nums[right], target)) {
+        left = pivot + 1;
+      } else {
+        right = pivot - 1;
+      }
+    }
+  }
+
+  return -1;
+}
 
 module.exports = search;
