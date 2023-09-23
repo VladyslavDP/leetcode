ee555647f0e45c1c03bc17f782dc22dddeb611c6
Vladyslav Sharapat <>
2023-09-23 23:46:13 +0300

    Added removeNthFromEnd for linked list;

--- linkedLists/LC19_removeNthFromEnd/index.js
+++ linkedLists/LC19_removeNthFromEnd/index.js
@@ -1,3 +1,28 @@
-function removeNthFromEnd(head, n) {}
+const {convertArrToLL, fetchLLVals} = require('./../utility/utility');
+
+const startingLL = convertArrToLL([5, 4, 3, 2, 1]);
+
+function removeNthFromEnd(head, n) {
+    let right = head;
+    let left = head;
+
+    for (let i = 0; i < n; i++) {
+        right = right.next;
+    }
+
+    if (!right) {
+        return head.next;
+    }
+
+    while (right.next != null) {
+        right = right.next;
+        left = left.next;
+    }
+
+    left.next = left.next.next;
+
+    return head;
+
+}
 
 module.exports = removeNthFromEnd;
